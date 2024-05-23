5fb81b90120cf1af12bffc4b35eb37f2b052f515
SM-junior <shahinalam12341@gmail.com>
2024-05-23 15:02:02 +0600

    eslint is not working

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,14 +6,7 @@ import globals from "globals";
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
-  {
-    rules: {
-        "no-unused-vars": "error",
-        "no-undef": "error",
-        "prefer-const":"error",
-        "no-console":'warn'
-    }
-},
+  
 {
     ignores:["**/node_modules/","**/dist/"]
 },
